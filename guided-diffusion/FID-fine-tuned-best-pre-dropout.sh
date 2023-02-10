284cb24e44f4acfa1f2b06218943bad8adb46a9c
alexakp <alexakp@student.matnat.uio.no>
2023-02-11 00:56:18 +0100

    fid update

--- guided-diffusion/FID-fine-tuned-best-pre-dropout.sh
+++ guided-diffusion/FID-fine-tuned-best-pre-dropout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in {002000..040000..2000}
+for i in {022000..040000..2000}
 do
   #echo Model nr: $i
   python -m pytorch_fid D:/Simula_data/Images_data/images/polyps-3 C:/Users/Alexander-PC-hjemme/Desktop/UiO/Master_Thesis/guided-diffusion/generated-fine-tuned-best-pre-0.3-dropout/$i
