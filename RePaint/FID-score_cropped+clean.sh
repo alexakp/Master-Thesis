be799a35da02868ebf547550105cea3c47923f5e
alexakp <alexakp@student.matnat.uio.no>
2023-02-15 13:16:53 +0100

    deleted

--- RePaint/FID-score_cropped+clean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -m pytorch_fid D:/Simula_data/Images_data/images/polyps-3 C:/Users/Alexander-PC-hjemme/Desktop/UiO/Master_Thesis/RePaint/log/inpaint_background_clean/inpainted
-
--- RePaint/split_up_dataset.py
+++
@@ -1,2 +0,0 @@
-import splitfolders
-splitfolders.ratio('D:/Simula_data/unlabeled-dataset-128x128/temp', output="D:/Simula_data/unlabeled-dataset-128x128", seed=1337, ratio=(.95, 0,0.05)) 
